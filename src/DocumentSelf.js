648082041d9df3e7a28d6fd8e8768124146377df
cmc19 <carisch19@gmail.com>
2014-02-13 23:26:20 -0500

    doc

--- src/DocumentSelf.js
+++ src/DocumentSelf.js
@@ -11,7 +11,7 @@ function init() {
                     // * missing semicolons are added
                     sampleCode();
 
-                    var badFormating = wth;
+                    var badFormating = function () { var x = 1; x++; /*this is one long line in the src file */ var y = x; }
                 }
             })
         }
@@ -33,13 +33,13 @@ function init() {
 
     doc.codeSample('Sample Code', 'This is sample code', function () {
         function intellisenseDocumentation(arg1, arg2, arg3, optionalArg) {
-        	/// <summary>
-        	/// Summary about the function
-        	/// </summary>
-        	/// <param name="arg1">about arg1</param>
-        	/// <param name="arg2"></param>
-        	/// <param name="arg3"></param>
-            /// <param name="optionalArg" optional="true"></param>
+            /// <summary>
+            /// Summary about the function
+            /// </summary>
+            /// <param name="arg1">about arg1</param>
+            /// <param name="arg2"></param>
+            /// <param name="arg3"></param>
+            /// <param name="optionalArg" optional="true">This is optional</param>
             dostuff();
 
         }
